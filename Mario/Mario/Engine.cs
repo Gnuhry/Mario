3591d12af4e3038e3a124177cb455fac855bdd79
Gnuhry <truongxh1@gmail.com>
2018-10-13 21:03:10 +0200

    ItemBox

--- Mario/Mario/Engine.cs
+++ Mario/Mario/Engine.cs
@@ -8,17 +8,21 @@ namespace Mario
     public class Engine
     {
         private Control[,] controls;
-        private Rectangle[] obstacles;
+        private Rectangle[] obstacles, itemboxes;
+        private List<Rectangle> item;
         private Player player;
         private Timer timer;
-        private int pointer,gameWidth;
+        private int pointer, gameWidth;
         private Items items;
+        private Control.ControlCollection controlCollection;
 
-        public Engine(Control[] controls, Player player, Items items)
+        public Engine(Control[,] controls, Player player, Items items, Control.ControlCollection controlCollection)
         {
             obstacles = new Rectangle[] { new Rectangle(0, 400, 1000, 1) };
+            item = new List<Rectangle>();
             this.controls = controls;
             this.player = player;
+            this.controlCollection = controlCollection;
             pointer = 0;
             gameWidth = Screen.PrimaryScreen.WorkingArea.Width / Settings.width;
             this.items = items;
@@ -31,13 +35,36 @@ namespace Mario
         {
             foreach (Rectangle obstacle in obstacles)
             {
-                if (rectangle.IntersectsWith(obstacle)) return false;
+                if (rectangle.IntersectsWith(obstacle))
+                    return false;
+            }
+            foreach (Rectangle itembox in itemboxes)
+            {
+                if (rectangle.IntersectsWith(itembox))
+                    return false;
+            }
+            for (int f = 0; f < item.Count; f++)
+            {
+                if (rectangle.IntersectsWith(item[f]))
+                {
+                    items.pickUpItem(f, controlCollection);
+                    item.RemoveAt(f);
+                    return true;
+                }
             }
             return true;
         }
-        private void SetObstacles(Control[]controls)
+        private bool itemBox(Rectangle rectangle)
         {
-            List<Rectangle> rectangles = new List<Rectangle>();
+            foreach (Rectangle itembox in itemboxes)
+            {
+                if (rectangle.IntersectsWith(itembox)) return true;
+            }
+            return false;
+        }
+        private void SetObstacles(Control[] controls)
+        {
+            List<Rectangle> rectangles = new List<Rectangle>(), rectangles2 = new List<Rectangle>();
 
             foreach (Control control in controls)
             {
@@ -45,20 +72,25 @@ namespace Mario
                 {
                     rectangles.Add(new Rectangle(control.Location, control.Size));
                 }
+                else if (control.Tag.Equals("itembox"))
+                {
+                    rectangles2.Add(new Rectangle(control.Location, control.Size));
+                }
             }
             rectangles.Add(new Rectangle(0, 400, 1000, 1));
             obstacles = rectangles.ToArray();
+            itemboxes = rectangles2.ToArray();
         }
-        public void DisplayBackground(Control.ControlCollection controlCollection)
+        public void DisplayBackground()
         {
             List<Control> gameControls = new List<Control>();
-            for (int row = pointer;row < gameWidth&& row < 10;row++)
+            for (int row = pointer; row < gameWidth && row < 10; row++)
             {
-                for(int column = 0;column < Settings.gamehight&& column < 3;column++)
+                for (int column = 0; column < Settings.gamehight && column < 3; column++)
                 {
 
                     gameControls.Add(controls[row, column]);
-                    controls[row, column].Location = new Point((row - pointer)*Settings.width, column*Settings.hight);
+                    controls[row, column].Location = new Point((row - pointer) * Settings.width, column * Settings.hight);
                     controlCollection.Add(controls[row, column]);
                 }
             }
@@ -69,20 +101,20 @@ namespace Mario
             //die anderen nicht hinzufügen
             //SetObstacles();
         }
-        private void MoveBackgroundLeft(ref Control.ControlCollection controlCollection)
+        private void MoveBackgroundLeft()
         {
             //TODO alles nach rechts Move
             //ganz rechts die Controls löschen
             //links neue Controls hinzufügen
         }
-        private void MoveBackgroundRight(ref Control.ControlCollection controlCollection)
+        private void MoveBackgroundRight()
         {
             //TODO alles nach links Move
             //ganz links die Controls löschen
             //rechts neue Controls hinzufügen
         }
-       
-        
+
+
         private void Timer(object sender, EventArgs e)
         {
             int x = 0, y = 0;
@@ -96,6 +128,10 @@ namespace Mario
                 if (collisionDetect(new Rectangle(player.control.Location.X, player.control.Location.Y - Settings.speedX, player.control.Size.Width, player.control.Size.Height)))
                 {
                     x = -Settings.speedX;
+                }
+                else if (itemBox(new Rectangle(player.control.Location.X, player.control.Location.Y - Settings.speedX, player.control.Size.Width, player.control.Size.Height)))
+                {
+                    item.Add(items.getRandomItem(controlCollection, player.control.Location));
                 }
             }
             else
--- Mario/Mario/Items.cs
+++ Mario/Mario/Items.cs
@@ -1,43 +1,70 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace Mario
 {
     public class Items
     {
+        private List<Control> item_control;
+        private List<int> item_value;
         private bool Mushroom_;
-        private int Item, JumpCounter;
+        private int Item_, JumpCounter;
 
-        public int getRandomItem()
+        public int Item
         {
-            Random rnd = new Random();
-            return rnd.Next(1, 4);
+            get => Item_;
+            set => Item_ = value;
         }
 
-        public void useItem()
+        public Rectangle getRandomItem(Control.ControlCollection controlCollection, Point item_location)
         {
-            switch (Item)
+            Random rnd = new Random();
+            PictureBox pictureBox = new PictureBox();
+            pictureBox.Size = new Size(Settings.width, Settings.hight);
+            int item_rnd = rnd.Next(1, 4);
+            pictureBox.Image = Image.FromFile(getItemPicture(item_rnd));
+            item_location.Offset(0, -2 * Settings.speedX);
+            pictureBox.Location = item_location;
+            controlCollection.Add(pictureBox);
+            item_control.Add(pictureBox);
+            item_value.Add(item_rnd);
+            return new Rectangle(item_location, new Size(Settings.width, Settings.hight));
+        }
+        public void pickUpItem(int id, Control.ControlCollection controlCollection)
+        {
+            controlCollection.Remove(item_control[id]);
+            item_control.RemoveAt(id);
+            Item_ = item_value[id];
+            item_value.RemoveAt(id);
+        }
+
+        private string getItemPicture(int Item_)
+        {
+            string path = Environment.CurrentDirectory.Remove(Environment.CurrentDirectory.Length - 9) + "img\\";
+            switch (Item_)
             {
-                case 0: return;//noItem
-                case 1: return;//DoubleJump
-                case 2: return;//FireFlower
-                case 3: return;//Invincible
+                case 1: return path + "doublejump.jpg";//DoubleJump
+                case 2: return path + "fireflower.jpg";//FireFlower
+                case 3: return path + "invincible.jpg";//Invincible
             }
+            return null;
         }
         public bool FireFlower
         {
-            get => Item == 2;
+            get => Item_ == 2;
         }
         public bool Invincible
         {
-            get => Item == 3;
+            get => Item_ == 3;
         }
         public bool DoubleJump
         {
-            get => Item == 1 && (JumpCounter++) % 2 == 0;
+            get => Item_ == 1 && (JumpCounter++) % 2 == 0;
             set => JumpCounter = 0;
         }
 
@@ -52,7 +79,7 @@ namespace Mario
         public Items()
         {
             JumpCounter = 0;
-            Item = 1;
+            Item_ = 1;
         }
     }
 }

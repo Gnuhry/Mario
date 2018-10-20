8c55c142de2ae7fca008da7f5c7e3310fd78f69b
Max Kerkmann <max.p.kerkmann@gmail.com>
2018-10-20 20:19:22 +0200

    Verschieben

--- Mario/Mario/Engine.cs
+++ Mario/Mario/Engine.cs
@@ -8,6 +8,7 @@ namespace Mario
     {
         private Control[,] controls;
         private int pointer, gameWidth;
+        private double border;
         List<Control> gameControls = new List<Control>();
         private Control.ControlCollection controlCollection;
 
@@ -17,6 +18,7 @@ namespace Mario
             this.controlCollection = controlCollection;
             pointer = 0;
             gameWidth = Screen.PrimaryScreen.WorkingArea.Width / Settings.width;
+            border = gameWidth * 0.20;
             DisplayBackground();
         }
         private void DisplayBackground()
@@ -43,15 +45,64 @@ namespace Mario
         }
         private void MoveBackgroundLeft()
         {
+            pointer--;
+            Point help = new Point();
+            help.X = 50; // Position des Spielers, die ich nicht finde 
+            if (help.X >= border && pointer > 0)
+            {
+                for (int row = gameWidth; row < gameWidth - 1; row--)
+                {
+                    for (int column = 0; column <= Settings.gamehight; column++)
+                    {
+                        controlCollection.Remove(controls[row, column]); //Entfernen
+                        gameControls.Remove(controls[row, column]);
+                        for (int i = 0; controlCollection.Count >= i; i++) // Verschieben
+                        {
+                            controlCollection[i].Location = new Point(controlCollection[i].Location.X - Settings.width, controlCollection[i].Location.Y - Settings.height);
+                        }
+
+                        gameControls.Add(controls[row, column]);    //Neu hinzufügen
+                        controls[row, column].Location = new Point((row - (pointer - 1)) * Settings.width, column * Settings.height);
+                        controlCollection.Add(controls[row, column]);
+                    }
+                }
+            }
+
+
             //TODO alles nach rechts Move
             //ganz rechts die Controls löschen
             //links neue Controls hinzufügen
         }
         private void MoveBackgroundRight()
         {
+            pointer++;
+            Point help = new Point();
+            help.X = 50; // Position des Spielers, die ich nicht finde 
+            if (help.X >= gameWidth - border)
+            {
+                for (int row = gameWidth - (gameWidth - (pointer - 1)); row < row + 1; row++)
+                {
+                    for (int column = 0; column <= Settings.gamehight; column++)
+                    {
+                        controlCollection.Remove(controls[row, column]);  //Entfernen
+                        gameControls.Remove(controls[row, column]);
+
+                        for (int i = 0; controlCollection.Count >= i; i++) // Verschieben
+                        {
+                            controlCollection[i].Location = new Point(controlCollection[i].Location.X + Settings.width, controlCollection[i].Location.Y + Settings.height);
+                        }
+                        gameControls.Add(controls[row, column]);    //Neue Reihe hinzufügen
+                        controls[row, column].Location = new Point((row - (pointer - 1)) * Settings.width, column * Settings.height);
+                        controlCollection.Add(controls[row, column]);
+
+                    }
+                }
+            }
+
             //TODO alles nach links Move
             //ganz links die Controls löschen
             //rechts neue Controls hinzufügen
         }
     }
 }
+
